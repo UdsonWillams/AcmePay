4c5f201b899c70b78c36c037c8b4f0dcd47c12f8
UdsonWillams <udson.willams@gmail.com>
2024-04-25 21:51:37 -0300

    adicionando app

---
+++ src/main/java/br/com/acme_pay/App.java
@@ -0,0 +1,13 @@
+package br.com.acme_pay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
