1b2a0c89f85f287891a708c4a873a6d2e2546598
UdsonWillams <udson.willams@gmail.com>
2024-04-25 21:50:48 -0300

    adicionando o valor das classes e tipos.

--- src/main/java/br/com/acme_pay/domain/Account.java
+++ src/main/java/br/com/acme_pay/domain/Account.java
@@ -1,4 +1,30 @@
 package br.com.acme_pay.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
+
+    private Long id;
+    private Integer number;
+    private Integer agency;
+    private BigDecimal balance;
+    private Boolean close;
+    private Customer customer;
+    private List<Card> cards;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    public Account create() {
+        // TODO: 27/04/24
+        return this;
+    }
 }
--- src/main/java/br/com/acme_pay/domain/Card.java
+++ src/main/java/br/com/acme_pay/domain/Card.java
@@ -1,4 +1,21 @@
 package br.com.acme_pay.domain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Card {
+
+    private Long id;
+    private String flag;
+    private BigDecimal card_limit;
+    private Account account;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
 }
